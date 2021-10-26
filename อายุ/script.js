fac7c3f08d45b81d8bad9935dc3dd187f7e2a516
BlackW00dCC <92838846+BlackW00dCC@users.noreply.github.com>
2021-10-26 13:08:01 +0700

    Add files via upload

---
+++ อายุ/script.js
@@ -0,0 +1,14 @@
+var age =(prompt(""))
+console.log("อายุที่ใส่ "+age)
+console.log(+age>=18)
+
+
+if(age>=18){
+document.getElementById("content").innerHTML = "ฮั่นแน่แก่แล้วซินะ"
+
+}else
+{
+
+document.getElementById("content").innerHTML = "เจ้ายังเด็กนักเจ้าหนู"
+
+}
